6e27b1391d73cde17d60ad7c4ca3a333b54c2fc5
vivek <vivek@vp-server.local>
2023-12-09 14:08:49 -0500

    AOC9

---
+++ aoc9.hs
@@ -0,0 +1,28 @@
+import Text.Parsec
+import Text.Parsec.Text (Parser, parseFromFile)
+
+main :: IO ()
+main = do
+  f <- parseFromFile aocFile "aoc9.txt"
+  case f of
+    Left err -> print err
+    Right e -> do
+        putStrLn "AOC9 Answer 1:"
+        print $ sum $ (\x -> round $ lagrangeInterpolate (zip [1..] x) ((fromIntegral $ length $ x) + 1)) <$> e
+        putStrLn "AOC9 Answer 2:"
+        print $ sum $ (\x -> round $ lagrangeInterpolate (zip [1..] x) 0) <$> e
+
+lagrangeInterpolate :: [(Double, Double)] -> Double -> Double
+lagrangeInterpolate points x = sum [y * product [(x - xj) / (xi - xj) | (xj, _) <- points, xj /= xi] | (xi, y) <- points]
+
+aocFile :: Parser [[Double]]
+aocFile = aocLine `sepEndBy1` endOfLine
+
+aocLine :: Parser [Double]
+aocLine = do
+    ints `sepEndBy1` many1 (char ' ')
+
+ints :: Parser Double
+ints = do
+    x <- many1 (oneOf "1234567890-")
+    return $ read x
