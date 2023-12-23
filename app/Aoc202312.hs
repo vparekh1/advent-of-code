dfbf28b58a39964c6838bf9018f0341dd9eafa4b
vivek <vivek@vp-server.local>
2023-12-23 11:56:12 -0500

    AOC12 done

--- app/Aoc202312.hs
+++ app/Aoc202312.hs
@@ -1,45 +1,41 @@
-{-# LANGUAGE ViewPatterns #-}
-
 module Aoc202312 (solve) where
 
-import Data.HashMap.Strict (HashMap)
-import Data.Vector.Unboxed as V
-import Debug.Trace
+import Data.MemoTrie as T
 import Parse (printSolution)
 import Text.Parsec
 import Text.Parsec.ByteString (Parser)
 
 solve :: IO ()
-solve = printSolution aocFile aocFile "data/aoc12test.txt" (\g -> countPermutations <$> g) (const "")
+solve = printSolution aocFile aocFile "data/aoc12.txt" (\g -> sum $ countPerm <$> g) (\g -> sum $ countPerm . quintuple <$> g)
 
 type QueryItem = Char
 
-type Query = V.Vector QueryItem
+type Query = [QueryItem]
 
-type Answer = V.Vector Int
+type Answer = [Int]
 
-countPermutations :: (Query, Answer) -> Int
-countPermutations (V.uncons -> Nothing, V.uncons -> Nothing) = 1
-countPermutations (V.uncons -> Nothing, V.uncons -> Just (a, as))
-  | a == 0 && V.null as = 1
-  | otherwise = 0
-countPermutations (V.uncons -> Just (q, qs), V.uncons -> Nothing)
-  | q == '.' || q == '?' = countPermutations (qs, V.empty)
-  | otherwise = 0
-countPermutations (V.uncons -> Just (q, qs), V.uncons -> Just (a, as))
-  | a + V.sum as + V.length as - 1 > V.length qs + 1 = 0
-  | a == 0 && V.null as = countPermutations (q `cons` qs, V.empty)
-  | q == '.' = case a of
-      0 -> countPermutations (qs, as)
-      _ -> countPermutations (qs, fulla)
-  | q == '#' = case a of
-      0 -> 0
-      _ -> countPermutations (qs, (a - 1) `cons` as)
-  | q == '?' = countPermutations ('.' `cons` qs, fulla) + countPermutations ('#' `cons` qs, fulla)
-  | otherwise = 0
+countPerm :: (Query, Answer) -> Int
+countPerm (query, answer) = countPerm' '.' query answer
   where
-    fulla = a `cons` as
-countPermutations (_, _) = 0
+    recurse = T.memo3 countPerm'
+    countPerm' _ [] [] = 1
+    countPerm' _ [] (_ : _) = 0
+    countPerm' _ qss [] = if '#' `elem` qss then 0 else 1
+    countPerm' ch (q : qs) (a : as)
+      | a + sum as + length as > length qs + 1 = 0
+      | otherwise = case q of
+          '.' -> recurse '.' qs (a : as)
+          '#' ->
+            let takes = '.' `notElem` take (a - 1) qs
+                drops = drop (a - 1) qs
+             in if ch /= '#' && length qs >= (a - 1) && takes then recurse '#' drops as else 0
+          '?' -> recurse ch ('.' : qs) (a : as) + recurse ch ('#' : qs) (a : as)
+          _ -> 0
+
+quintuple :: (Query, Answer) -> (Query, Answer)
+quintuple (q, a) =
+  let qq = q ++ ['?']
+   in (qq ++ qq ++ qq ++ qq ++ q, a ++ a ++ a ++ a ++ a)
 
 aocFile :: Parser [(Query, Answer)]
 aocFile = parseInteresting `sepEndBy1` endOfLine
@@ -49,4 +45,4 @@ parseInteresting = do
   query <- many1 (oneOf "?#.")
   spaces
   answer <- (read <$> many1 digit) `sepBy` char ','
-  return (V.fromList query, V.fromList answer)
+  return (query, answer)
