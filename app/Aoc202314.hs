e275eac4998296e66c97bb04b575425d5a674598
vivek <vivek@vp-server.local>
2023-12-23 22:49:26 -0500

    AOC14 done

--- app/Aoc202314.hs
+++ app/Aoc202314.hs
@@ -1,7 +1,6 @@
-{-# LANGUAGE ViewPatterns #-}
-
 module Aoc202314 (solve) where
 
+import Data.Map as M
 import qualified Data.Maybe as Maybe
 import Debug.Trace
 import Parse
@@ -9,39 +8,56 @@ import Text.Parsec
 import Text.Parsec.ByteString (Parser)
 
 solve :: IO ()
-solve = printSolution aocFile aocFile "data/aoc14.txt" (fmap (weight . rollUp)) (const "")
+solve = printSolution aocFile aocFile "data/aoc14.txt" (fmap (weight . rollUp)) (fmap $ weight . mspin 1000000000)
 
-weight m = sum $ (uncurry (\x v -> (length $ filter (== 'O') v) * x)) <$> zip [1 ..] (reverse m)
+mspin :: Int -> [[Char]] -> [[Char]]
+mspin = go M.empty
+  where
+    go set count m
+      | count == 0 = traceShow m m
+      | otherwise = case set M.!? m of
+          Nothing -> go (M.insert m count set) (count - 1) (spin m)
+          Just x -> traceShow (x, count, count `rem` (x - count)) mspin (count `rem` (x - count)) m
 
+spin :: [[Char]] -> [[Char]]
+spin = rollRight . rollDown . rollLeft . rollUp
+
+weight :: [[Char]] -> Int
+weight m = sum $ (\(x, v) -> length (Prelude.filter (== 'O') v) * x) <$> zip [1 ..] (reverse m)
+
+rollRight :: [[Char]] -> [[Char]]
 rollRight m = rollRight1 <$> m
 
-rollLeft m = (reverse . rollRight1 . reverse) <$> m
+rollLeft :: [[Char]] -> [[Char]]
+rollLeft m = reverse . rollRight1 . reverse <$> m
 
-rollDown m = transpose $ rollRight1 <$> (transpose m)
+rollDown :: [[Char]] -> [[Char]]
+rollDown m = transpose $ rollRight1 <$> transpose m
 
-rollUp m = transpose $ (reverse . rollRight1 . reverse) <$> (transpose m)
+rollUp :: [[Char]] -> [[Char]]
+rollUp m = transpose $ reverse . rollRight1 . reverse <$> transpose m
 
 transpose :: [[a]] -> [[a]]
 transpose v =
-  [ [ (v !! col) !! row
+  [ [ v !! col !! row
       | col <- [0 .. maxRow]
     ]
     | row <- [0 .. maxCol]
   ]
   where
     maxRow = length v - 1
-    maxCol = length (v !! 0) - 1
+    maxCol = length (head v) - 1
 
-rollRight1 :: [Char] -> [Char]
+rollRight1 :: String -> String
 rollRight1 [] = []
 rollRight1 v =
   let untilHash = takeWhile (/= '#') v
       rem1 = dropWhile (/= '#') v
       hashes = takeWhile (== '#') rem1
       remainder = dropWhile (== '#') rem1
-      dots = length $ filter (== '.') untilHash
-      zeros = length $ filter (== 'O') untilHash
+      dots = length $ Prelude.filter (== '.') untilHash
+      zeros = length $ Prelude.filter (== 'O') untilHash
    in replicate dots '.' ++ replicate zeros 'O' ++ hashes ++ rollRight1 remainder
 
-aocFile :: Parser [[[Char]]]
+aocFile :: Parser [[String]]
 aocFile = parseGridList (noneOf " \n\r") (oneOf "#.O") `sepEndBy1` many1 endOfLine
