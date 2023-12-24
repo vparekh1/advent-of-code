07c6871baea94e50e73527cee571130578b9276e
vivek <vivek@vp-server.local>
2023-12-23 20:34:54 -0500

    AOC13 done

--- app/Aoc202313.hs
+++ app/Aoc202313.hs
@@ -1,8 +1,6 @@
 module Aoc202313 (solve) where
 
-import Data.HashMap.Internal.Strict
 import qualified Data.Maybe as Maybe
-import Data.MemoTrie as T
 import Data.Vector as V
 import Parse
 import Text.Parsec
@@ -11,12 +9,13 @@ import Text.Parsec.ByteString (Parser)
 type Matrix = Vector (Vector Char)
 
 solve :: IO ()
-solve = printSolution aocFile aocFile "data/aoc13.txt" ans1 (const "")
+solve = printSolution aocFile aocFile "data/aoc13.txt" (ans isReflect) (ans offByOneReflect)
 
-ans1 :: [Matrix] -> Int
-ans1 g =
-  let vertList = firstReflection . transpose <$> g
-      horizList = firstReflection <$> g
+ans :: (Vector (Vector Char) -> Int -> Bool) -> [Matrix] -> Int
+ans singleReflect g =
+  let reflection m = V.filter (singleReflect m) (V.fromList [1 .. (V.length m - 1)]) V.!? 0
+      vertList = reflection . transpose <$> g
+      horizList = reflection <$> g
       vertNums = Maybe.catMaybes vertList
       horizNums = Maybe.catMaybes horizList
    in Prelude.sum vertNums + Prelude.sum horizNums * 100
@@ -35,16 +34,15 @@ transpose v =
     maxCol = V.length (v V.! 0) - 1
 
 offByOneReflect :: Matrix -> Int -> Bool
-offByOneReflect mat n = go (n - 1) n mat
+offByOneReflect mat n = (go 0 (n - 1) n mat) == 1
   where
     maxRow = V.length mat - 1
-    go low high m
-      | low < 0 || high > maxRow = True
-      | m V.! low == m V.! high = go (low - 1) (high + 1) m
-      | otherwise = False
-
-firstReflection :: Matrix -> Maybe Int
-firstReflection m = V.filter (isReflect m) (V.fromList [1 .. (V.length m - 1)]) V.!? 0
+    go :: Int -> Int -> Int -> Matrix -> Int
+    go acc low high m
+      | low < 0 || high > maxRow = acc
+      | otherwise = go (acc + mismatches) (low - 1) (high + 1) m
+      where
+        mismatches = V.length $ V.filter (uncurry (/=)) (V.zip (m V.! low) (m V.! high))
 
 isReflect :: Matrix -> Int -> Bool
 isReflect mat n = go (n - 1) n mat
